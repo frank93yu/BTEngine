b7833def8b8bb41b31440b94311f89cc790f5511
frank3013yu <frank@trinnaclefund.com>
2017-12-02 17:40:18 -0500

    Simulation strategy is added with different assumed hitrate Latest quarter's signal is included in the subrevenue backtest Bug is fixed: close all and enter position on the same day

--- btEngine/backtestEngine.py
+++ btEngine/backtestEngine.py
@@ -13,18 +13,18 @@ import time
 
 def initialize_DB_connection():
     # establish database connection for daily and minute data
-    minutePriceDB = MyDBApi_GeoMinute({'user': 'root', 'password': '933900',\
+    minutePriceDB = MyDBApi_GeoMinute({'user': 'root', 'password': 'trinnacle17',\
         'host': '127.0.0.1', 'database': 'GeoTickersMinute15_17',})
     minutePriceDB.connect()
-    dailyPriceDB = MyDBApi_GeoDaily({'user': 'root', 'password': '933900',\
-        'host': '127.0.0.1', 'database': 'GeoTickersDaily15_17',})
+    dailyPriceDB = MyDBApi_GeoDaily({'user': 'root', 'password': 'trinnacle17',\
+        'host': '127.0.0.1', 'database': 'GeoTickersDaily07_17',})
     dailyPriceDB.connect()
-    dailyBetaSPYDB = MyDBApi_GeoDaily({'user': 'root', 'password': '933900',\
-        'host': '127.0.0.1', 'database': 'GeoTickersBetaSPY15_17',})
+    dailyBetaSPYDB = MyDBApi_GeoDaily({'user': 'root', 'password': 'trinnacle17',\
+        'host': '127.0.0.1', 'database': 'GeoTickersBetaSPY07_17',})
     dailyBetaSPYDB.connect()
     return dailyPriceDB, minutePriceDB, dailyBetaSPYDB
 
-def get_ticker_universe(path='/media/frank/SharedDisk/BTEngine/btEngine/allTickers.csv'):
+def get_ticker_universe(path='/media/trinnacle/86C6-B046/BTEngineTrinnacle/btEngine/allTickers.csv'):
     # get all the tickers in the backtest universe
     tempTickerList = pd.DataFrame.from_csv(path)
     tempTickerList = list(tempTickerList.index.values)
@@ -46,6 +46,7 @@ class MyBacktestEngine():
         self.allTickersDailyCloseDF = None # dataframe of all the close prices of all tickers in the universe
         self.ticker2index = None # A dict map ticker names to their corresponding column index in the numpy array below
         self.allTickersDailyCloseNP = None # numpy array of all the close prices for all tickers, for better iteration speed
+        self.badTickers = {} # all tickers without data
 
         # initialized in initialize_positions method
         self.cash_position, self.total_position = None, None # np array containing cash postion series
@@ -88,6 +89,7 @@ class MyBacktestEngine():
                     iTickerDataFrame.columns = ['date', iTicker]
                     iTickerDataFrame = iTickerDataFrame.set_index('date')
                 except:
+                    self.badTickers[iTicker] = True
                     print('Data for ' + iTicker + ' not available for this time frame!')
                     self.write_log('Data for ' + iTicker + ' not available for time frame: ' + self.start_date + ' to ' + self.end_date + '.')
                 tempTickerData.append(iTickerDataFrame)
@@ -130,8 +132,8 @@ class MyBacktestEngine():
         for iDate in range(len(self.calendar)):
             print('---------------' + str(self.calendar[iDate]) + '--------------')
             print('cash:' + str(self.cash_position[iDate]))
-            #print(self.tickers_positions[iDate])
-            print(self.total_position[iDate])
+            # print(self.tickers_positions[iDate])
+            print('Net position: ' + str(self.total_position[iDate]))
             iOrderBatch = dailyStrategy.run(self.calendar[iDate])
             self.place_orders_batch(iDate, iOrderBatch)
             self.daily_settlement(iDate)
@@ -158,10 +160,25 @@ class MyBacktestEngine():
 
     def place_orders_batch(self, i_date, order_batch, use_minute_data=False):
 
+        # avoid order conflicts, cancel all orders for today for tickers with 'closs all' overwrite
+        tickers_to_close = {}
+        for iOrder in order_batch:
+            if iOrder['volume'] == 'close all':
+                tickers_to_close[iOrder['ticker']] = 1
+
         for iOrder in order_batch:
             iTicker = iOrder['ticker']
             iOrderAmount = iOrder['volume']
             use_weight = iOrder['use_weight']
+
+            # exclude 'close all' tickers for today
+            if (iTicker in tickers_to_close) and iOrderAmount != 'close all':
+                continue
+
+            # exclude tickers without data
+            if iTicker in self.badTickers:
+                continue
+
             if not use_minute_data:
                 iCost = self.allTickersDailyCloseNP[i_date][self.ticker2index[iTicker]]
             else:
@@ -172,16 +189,19 @@ class MyBacktestEngine():
                 iCost = 0
                 print('Fatal data missing for ' + iTicker + ' on ' + str(self.calendar[i_date]) + ', trade aborted.')
                 self.write_log('Fatal data missing for ' + iTicker + ' on ' + str(self.calendar[i_date]) + ', trade aborted.')
-            print(iOrderAmount)
+            # print(iOrderAmount)
             if iOrderAmount == "close all":
                 iOrderAmount = -self.tickers_positions[i_date][self.ticker2index[iTicker]]
             elif use_weight:
-                print("cash: " + str(self.cash_position[i_date]))
-                iOrderAmount = iOrderAmount*self.cash_position[i_date]/iCost
+                # print("cash: " + str(self.cash_position[i_date]))
+                if (self.cash_position[i_date] > 0):
+                    iOrderAmount = iOrderAmount*self.cash_position[i_date]/iCost
+                else:
+                    iOrderAmount = 0
             self.cash_position_taken[i_date] -= iCost*iOrderAmount
             self.tickers_positions_taken[i_date][self.ticker2index[iTicker]] += iOrderAmount
             self.all_trades.append([self.calendar[i_date], iTicker, iCost, str(iOrderAmount)])
-            print(str(self.calendar[i_date]) + ': ' + str(iOrderAmount) + ' shares of ' + iTicker + ' at ' + str(iCost) + '.')
+            print(str(iOrderAmount) + ' shares of ' + iTicker + ' at ' + str(iCost) + '.')
             # self.write_log(str(self.calendar[i_date]) + ': ' + str(iOrderAmount) + ' shares of ' + iTicker + ' at ' + str(iCost) + '.')
 
     def get_minute_mean(self, ticker, start_date, start_time, end_date, end_time):
@@ -250,7 +270,7 @@ class MyBacktestEngine():
             this_hedge_position += hedgingTickerPosition
 
         hedge_position_taken = this_hedge_position - last_hedge_position
-        print("thisHedge: " + str(this_hedge_position))
+        # print("thisHedge: " + str(this_hedge_position))
         hedge_order = [{'ticker': ticker, 'volume': hedge_position_taken, "start_time": "10:00:00", "end_time": "16:00:00", "use_weight": False}]
         print(str(self.calendar[i_date]) + ': ' + str(hedge_position_taken) + ' shares of ' + ticker + ' is taken for hedging.')
         return hedge_order
@@ -264,9 +284,16 @@ class MyBacktestEngine():
 class Strategy():
 
     def __init__(self):
-        self.subRevSignals = pd.DataFrame.from_csv("/media/frank/SharedDisk/BTEngine/subRevStrategy/oneday_subRevSignalBT10percent.csv")
-    def run(self, i_date):
+
+        self.subRevSignals = pd.DataFrame.from_csv("/media/trinnacle/86C6-B046/BTEngineTrinnacle/signals/subRevStrategy/oneday_subRevSignalBT10percent.csv")
+        self.preprocess()
+
+    def preprocess(self):
+
         self.subRevSignals['date'] = pd.to_datetime(self.subRevSignals['date'], format="%Y-%m-%d")
+
+    def run(self, i_date):
+
         thisBatch = self.subRevSignals[self.subRevSignals['date'] <= i_date]
         self.subRevSignals = self.subRevSignals[self.subRevSignals['date'] > i_date]
         orderList = []
@@ -283,7 +310,7 @@ class Strategy():
 # strategy.run(pd.to_datetime('2016-01-01', format="%Y-%m-%d"))
 
 a = MyBacktestEngine()
-a.create_calendar('2015-04-01', '2017-08-01')
+a.create_calendar('2015-03-01', '2017-11-30')
 a.load_daily_close()
 a.initialize_positions(1000000)
 strategy = Strategy()
